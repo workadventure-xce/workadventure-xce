b304b3e41c9110625f75920e8816023281ad8774
PizZaKatZe <pizzakatze@chaos-siegen.de>
2021-03-11 23:21:43 +0100

    Increase action button font size

--- front/src/Phaser/Game/GameScene.ts
+++ front/src/Phaser/Game/GameScene.ts
@@ -766,7 +766,7 @@ export class GameScene extends ResizableScene implements CenterListener {
                 if(jitsiTriggerValue && jitsiTriggerValue === ON_ACTION_TRIGGER_BUTTON) {
                     let message = allProps.get(JITSI_MESSAGE_PROPERTIES);
                     if (message === undefined) {
-                        message = 'Press SPACE or touch here to enter Jitsi Meet room';
+                        message = 'Press SPACE or touch here to enter video conference';
                     }
                     layoutManager.addActionButton('jitsiRoom', message.toString(), () => {
                         openJitsiRoomFunction();
