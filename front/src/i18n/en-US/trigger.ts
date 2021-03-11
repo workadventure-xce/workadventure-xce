511a51c08599d9fbe7a323f77226ebf19e3992fa
PizZaKatZe <pizzakatze@chaos-siegen.de>
2021-03-11 23:21:43 +0100

    Increase action button font size

--- front/src/i18n/en-US/trigger.ts
+++ front/src/i18n/en-US/trigger.ts
@@ -2,7 +2,7 @@ import type { BaseTranslation } from "../i18n-types";
 
 const trigger: BaseTranslation = {
     cowebsite: "Press SPACE or touch here to open web site",
-    jitsiRoom: "Press SPACE or touch here to enter Jitsi Meet room",
+    jitsiRoom: "Press SPACE or touch here to enter video conference",
     newTab: "Press SPACE or touch here to open web site in new tab",
 };
 
