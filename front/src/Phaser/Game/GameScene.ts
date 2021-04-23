29977c1f872ed971f1c483065d17aeedde2ce02f
TabascoEye <tabascoeye@gmail.com>
2021-04-24 01:17:43 +0200

    ignore "any" in linter

--- front/src/Phaser/Game/GameScene.ts
+++ front/src/Phaser/Game/GameScene.ts
@@ -129,7 +129,7 @@ interface MapProperty {
     value: string|number;
 }
 
-function getMapProperty(mapData: any, name: string, type: string, dflt: any = null): any {
+function getMapProperty(mapData: any, name: string, type: string, dflt: any = null): any { //eslint-disable-line @typescript-eslint/no-explicit-any
   if (!Array.isArray(mapData.properties)) {
     return dflt;
   }
